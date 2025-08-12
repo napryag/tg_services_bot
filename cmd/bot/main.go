c07f69f0f3c4529ab431d02786ca630111d87d66
Dmitriy Khalko <dxalko@gmail.com>
2025-08-12 19:48:01 +0300

    BOT-3

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,1 +1,38 @@
 package bot
+
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/napryag/tg_services_bot/pkg/domain/bot/reciever/config"
+	"github.com/rs/zerolog"
+)
+
+func main() {
+	cfg, err := config.LoadConfig()
+	var logger zerolog.Logger
+	bot, err := tgbotapi.NewBotAPI(cfg.BotToken)
+	if err != nil {
+		logger.Err(err)
+	}
+
+	bot.Debug = true
+
+	logger.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
+}
---
+++ pkg/domain/bot/reciever/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
+	"github.com/napryag/tg_services_bot/pkg/utils/errs"
+	"github.com/rs/zerolog"
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Logger      zerolog.Logger
+	PostgreAddr string `yaml:"posgre_addr" validate:"required"`
+	WebhookURL  string `yaml:"webhook_url" validate:"required"`
+	HTTPPort    int    `yaml:"http_port" validate:"required"`
+	WorkerCount int    `yaml:"worker_count" validate:"required"`
+	BotToken    string
+}
+
+func LoadConfig() (*Config, error) {
+	path := filepath.Join("cmd/bot/etc", "app.yml")
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, errs.New("failed to read config file").Wrap(err)
+	}
+
+	var cfg Config
+	if err = yaml.Unmarshal(data, &cfg); err != nil {
+		return nil, errs.New("failed to unmarshal YAML").Wrap(err)
+	}
+
+	// Validate
+	if err = validator.New().Struct(cfg); err != nil {
+
+		return nil, errs.New("config validation failed").Wrap(err)
+	}
+
+	if err = godotenv.Load(); err != nil {
+		return nil, errs.New("failed to load .env").Wrap(err)
+	}
+	cfg.BotToken = os.Getenv("TG_TOKEN")
+
+	return &cfg, nil
+}
